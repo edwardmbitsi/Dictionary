4e1d35ea6d564b276128839d70a78935ab37f481
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-11-25 17:43:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
